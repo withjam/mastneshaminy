486804a78b62626a76ad46ca6fd8aae9e9843d11
Matt Pileggi <mattpileggi@gmail.com>
2012-05-12 16:42:43 -0400

    test framework copied from rival

--- mast/tests.py
+++ mast/tests.py
@@ -1,16 +1,29 @@
 import unittest
+import pymongo
 
 from pyramid import testing
 
-class ViewTests(unittest.TestCase):
+db = None
+
+def getDB(dbname="masttest",uri="mongodb://staff.mongohq.com:10052"):
+    global db
+    if db is None:
+        conn = pymongo.Connection(uri)
+        db = conn[dbname]
+        db.authenticate('dbuser','dbpass')    
+    return db
+
+def getRequest(matchdict=None,**kvars):
+    request = testing.DummyRequest(**kvars)
+    request.db = getDB()
+    request.matchdict = matchdict
+    return request
+
+class AppTests(unittest.TestCase):
     def setUp(self):
+        getDB()
         self.config = testing.setUp()
 
     def tearDown(self):
         testing.tearDown()
 
-    def test_my_view(self):
-        from .views import my_view
-        request = testing.DummyRequest()
-        info = my_view(request)
-        self.assertEqual(info['project'], 'mast')
