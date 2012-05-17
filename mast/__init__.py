02edce63b69f8aae8dd2c861ebfaa4ed78575e7d
Matt Pileggi <mattpileggi@gmail.com>
2012-05-17 00:34:31 -0400

    change web routes to .html

--- mast/__init__.py
+++ mast/__init__.py
@@ -28,10 +28,10 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     # Web Routes
     config.add_route('home', '/')
-    config.add_route('realhome', '/home')
-    config.add_route('petition', '/petition')
+    config.add_route('realhome', '/home.html')
+    config.add_route('petition', '/petition.html')
     config.add_route('viewsign', '/signature/{filename}.png')
-    config.add_route('apply', '/apply')
+    config.add_route('apply', '/apply.html')
     # API Routes
     config.add_route('postApp','/json/applicant')
     config.add_route('postSign','/json/sign')
